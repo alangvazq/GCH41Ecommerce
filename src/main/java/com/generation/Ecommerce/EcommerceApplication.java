045dd47c7c4df5efefcbf3d035b3e20ca17d9e53
alangvazq <alan.g.vazq@gmail.com>
2024-08-04 02:18:46 -0600

    properties de prueba

--- src/main/java/com/generation/Ecommerce/EcommerceApplication.java
+++ src/main/java/com/generation/Ecommerce/EcommerceApplication.java
@@ -2,6 +2,10 @@ package com.generation.Ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.generation.Ecommerce.config.JwtFilter;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -10,4 +14,14 @@ public class EcommerceApplication {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<JwtFilter> UserJwtFilter() {
+		FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<JwtFilter>();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/api/productos/*");
+		registrationBean.addUrlPatterns("/api/usuarios/*");
+		registrationBean.addUrlPatterns("/api/ordenes/*");
+		registrationBean.addUrlPatterns("/api/orden-tiene-productos/*");
+		return registrationBean;
+	}
 }
